c9f2666d909f577d9d98d41133b7ab08f1cab6b2
OleksandrVosmushko <sasha.vosmushko@gmail.com>
2018-09-18 13:43:26 +0300

    init

---
+++ Taxi/Migrations/20180830134443_sl.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Taxi.Migrations
+{
+    public partial class sl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConnectionId",
+                table: "Drivers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConnectionId",
+                table: "Customers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConnectionId",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "ConnectionId",
+                table: "Customers");
+        }
+    }
+}
---
+++ Taxi/Migrations/20180904124231_drloc.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+namespace Taxi.Migrations
+{
+    public partial class drloc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Point>(
+                name: "Location",
+                table: "Drivers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Drivers");
+        }
+    }
+}
