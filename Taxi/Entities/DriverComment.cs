bed2eee947950f2ef1723eb557081cc17120eb82
Unknown <sasha.vosmushko@gmail.com>
2018-10-19 09:17:31 +0300

    hot hot fix

---
+++ Taxi/Entities/DriverComment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taxi.Entities
+{
+    public class DriverComment
+    {
+        public Guid Id { get; set; }
+
+        public string Comment { get; set; }
+
+        public Guid DriverId { get; set; }
+
+        public Driver Driver { get; set; }
+
+        public DateTime CreationTime { get; set; }
+    }
+}
